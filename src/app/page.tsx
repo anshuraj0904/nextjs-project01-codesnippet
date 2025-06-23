6f979c5b42e8c16cc5d4d546b5470c08e91ac7cb
anshuraj0904 <rajanshu0904@gmail.com>
2025-06-23 18:46:36 +0530

    Can view and delete the snippets now!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-export default function Home() {
+import { prisma } from "@/lib/prisma";
+
+export default async function Home() {
+  const snippets = await prisma.snippet.findMany();
+  if (!snippets || snippets.length === 0) {
+  }
+
   return (
-      <div>
-        <h1 className="text-4xl">Home Page</h1>
-        <div className="flex items-center justify-between">
-           <h1>Snippets</h1>
-           <Button><Link href='/snippet/new'>New</Link></Button>
-        </div>
+    <div>
+      <div className="flex items-center justify-between">
+      <h1 className="text-4xl">My Snippets</h1>
+        <Button>
+          <Link href="/snippet/new">New</Link>
+        </Button>
       </div>
+      {snippets.map((snippet) => (
+        <div
+          key={snippet.id}
+          className="bg-white flex justify-between  p-5 my-5 w-6/12 shadow-xl rounded-md"
+        >
+          <h2 className="text-2xl">{snippet.title}</h2>
+          <Link href={`snippet/${snippet.id}`}><Button className="mt-2 hover:text-blue-400 cursor-pointer">View</Button></Link>
+        </div>
+      ))}
+    </div>
   );
 }
---
+++ src/app/snippet/[id]/page.tsx
@@ -0,0 +1,62 @@
+import { prisma } from "@/lib/prisma";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
+
+
+/*
+// type snippetDetail = {
+//     params:Promise<{id:string}>
+// }
+
+--> We can also use this one for the type definition, but it's not necessary as we can directly use the type in the function parameter.
+*/
+
+async function codeCRUD(
+    
+  { params }: { params: Promise<{ id: string }> } // This type is defined. 
+) {
+  const id = parseInt((await params).id); // Extracting the id from the params.
+
+  const snippet = await prisma.snippet.findUnique({
+    where: {
+      id,
+    },
+  });
+
+
+  const handleDeleteSnippet = async()=>{
+    "use server";
+    await prisma.snippet.delete({
+      where: {
+        id,
+      },
+    });
+
+    redirect("/");
+  }
+
+  if (!snippet) {
+    return (
+      <div className="container mx-auto max-w-2xl p-5">
+        <h1 className="text-4xl">Snippet not found</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="container mx-auto max-w-2xl p-5 bg-white shadow-md rounded-md">
+      <h1 className="font-bold text-4xl text-center">{snippet.title}</h1>
+
+       <div className="flex gap-x-2 mt-4">
+      <Button variant={"outline"} className="cursor-pointer" >Edit</Button>
+      <Button variant={"destructive"} className="cursor-pointer" onClick={handleDeleteSnippet}>Delete</Button>
+       </div>
+
+      <pre className="bg-gray-900 text-green-300 font-mono text-sm p-4 rounded-lg overflow-x-auto mt-4 shadow-md">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
+}
+
+export default codeCRUD;
